f4df3d798c138ae435dd16d1391d77a0393cf8d9
NeutronX-dev <32855909+NeutronX-dev@users.noreply.github.com>
2021-03-01 09:33:06 -0500

    Source Code

---
+++ LimitlessInputs.py
@@ -0,0 +1,16 @@
+def KeyboardReader(type, default_text, stop_at):
+    response = []
+    while True:
+        inp = input(default_text)
+        try:
+            inp = int(inp)
+        except ValueError:
+            if(inp.lower() == "true"):
+                inp = True
+            elif (inp.lower() == "false"):
+                inp = False
+        if(str(inp) == str(stop_at)):
+            break
+        else:
+            response.append(inp)
+    return response
